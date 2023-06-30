a70068ae762ea5e976314ac204bb2fabe7d90d02
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2023-06-30 22:32:46 +0900

    Time: 11 ms (27.93%), Space: 43.2 MB (28.77%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(x);
+
+        return sb.toString().equals(sb.reverse().toString());
+    }
+}
