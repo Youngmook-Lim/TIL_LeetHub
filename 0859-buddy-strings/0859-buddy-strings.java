bed09ba3d1d01607dcd8badd0bc35fe38643ab29
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2023-07-03 21:20:31 +0900

    Time: 1 ms (99.01%), Space: 41.5 MB (72.80%) - LeetHub

--- 0859-buddy-strings/0859-buddy-strings.java
+++ 0859-buddy-strings/0859-buddy-strings.java
@@ -2,33 +2,30 @@ class Solution {
     public boolean buddyStrings(String s, String goal) {
         if (s.length() != goal.length()) return false;
         
+        if (s.equals(goal)) {
+            int[] alphabet = new int[26];
+            for (int i = 0; i < s.length(); i++) {
+                alphabet[s.charAt(i) - 'a']++;
+                if (alphabet[s.charAt(i) - 'a'] == 2) return true;
+            }
+            return false;
+        }
+        
         int n = s.length();
-        List<Character> sChars = new ArrayList<>();
-        List<Character> goalChars = new ArrayList<>();
-        int[] alphabet = new int[26];
+        List<Integer> list = new ArrayList<>();
         
         int cnt = 0;
-        boolean flag = false;
         
         for (int i = 0; i < n; i++) {
-            alphabet[s.charAt(i) - 'a']++;
-            if (alphabet[s.charAt(i) - 'a'] == 2) {
-                flag = true;
-            }
-            
             if (s.charAt(i) != goal.charAt(i)) {
                 cnt++;
-                sChars.add(s.charAt(i));
-                goalChars.add(goal.charAt(i));
+                list.add(i);
             }
             if (cnt > 2) return false;
         }
         
-        if (cnt == 0) return flag == true;
         if (cnt == 1) return false;
         
-        if (sChars.get(0) == goalChars.get(1) && sChars.get(1) == goalChars.get(0)) return true;
-        
-        return false;
+        return cnt == 2 && s.charAt(list.get(0)) == goal.charAt(list.get(1)) && s.charAt(list.get(1)) == goal.charAt(list.get(0));
     }
 }
