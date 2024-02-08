32b57bdb73b91886ba9f51a522bf6eca4180b636
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2024-02-09 00:07:25 +0900

    Time: 2 ms (10.43%), Space: 45.4 MB (26.62%) - LeetHub

--- 1920-build-array-from-permutation/1920-build-array-from-permutation.java
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.java
@@ -1,9 +1,21 @@
+import java.util.*;
+
 class Solution {
     public int[] buildArray(int[] nums) {
-        int[] ans = new int[nums.length];
-        for (int i = 0; i < nums.length; i++) {
-            ans[i] = nums[nums[i]];
+        int n = nums.length;
+        
+        // a = qb + r
+        for (int i = 0; i < n; i++) {
+            int r = nums[i];
+            int b = nums[nums[i]] % n;
+            
+            nums[i] = n * b + r;
         }
-        return ans;
+        
+        for (int i = 0; i < n; i++) {
+            nums[i] /= n;
+        }
+        
+        return nums;
     }
 }
