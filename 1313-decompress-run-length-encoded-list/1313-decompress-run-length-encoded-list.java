de3ddf49e9cab5c2153b93e603f1c493818bff3b
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2024-05-11 12:52:09 +0900

    Time: 4 ms (61.28%), Space: 45.1 MB (44.24%) - LeetHub

---
+++ 1313-decompress-run-length-encoded-list/1313-decompress-run-length-encoded-list.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int[] decompressRLElist(int[] nums) {
+        List<Integer> list = new ArrayList<>();  
+        for (int i = 0; i < nums.length; i += 2) {
+            int freq = nums[i];
+            int val = nums[i + 1];
+            for (int j = 0; j < freq; j++) {
+                list.add(val);
+            }
+        }
+        int[] ret = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            ret[i] = list.get(i);
+        }
+        return ret;
+    }
+}
