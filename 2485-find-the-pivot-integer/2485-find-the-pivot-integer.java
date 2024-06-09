8f1f0b1c29e5a7128502033ed2ec697a08f782b4
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2024-06-09 11:43:42 +0900

    Time: 0 ms (100.00%), Space: 40.1 MB (90.49%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.java
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.java
@@ -2,17 +2,10 @@ import java.util.*;
 
 class Solution {
     public int pivotInteger(int n) {
-        int[] arr = new int[n + 1];
-        for (int i = 1; i <= n; i++) {
-            arr[i] = arr[i - 1] + i;
-        }
         
-        for (int i = 1; i <= n; i++) {
-            int frontSum = arr[i];
-            int backSum = arr[n] - arr[i] + i;
-            if (frontSum == backSum) return i;
-        }
-        
+        double sumSquared = (double) (n * (n + 1) / 2);
+        int sum = (int) Math.sqrt(sumSquared);
+        if (sum * sum == sumSquared) return sum;
         return -1;
     }
 }
