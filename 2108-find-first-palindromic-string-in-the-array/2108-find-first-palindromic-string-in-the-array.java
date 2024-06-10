0181acbfa81867912b66f2f58eafbdacfc78a14e
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2024-06-10 19:51:21 +0900

    Time: 4 ms (35.87%), Space: 45.2 MB (20.79%) - LeetHub

--- 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.java
+++ 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.java
@@ -1,16 +1,10 @@
 class Solution {
     public String firstPalindrome(String[] words) {
         for (String w : words) {
-            if (isPalindrome(w)) return w;
+            String r = new StringBuilder(w).reverse().toString();
+            if (w.equals(r)) return w;
         }
         return "";
     }
     
-    public boolean isPalindrome(String s) {
-        int n = s.length();
-        for (int i = 0; i < n / 2; i++) {
-            if (s.charAt(i) != s.charAt(n - i - 1)) return false;
-        }
-        return true;
-    }
 }
