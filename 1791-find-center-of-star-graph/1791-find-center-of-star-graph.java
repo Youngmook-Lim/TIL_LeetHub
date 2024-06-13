869f703d60ca1eea88e73b65d65eedeb3ce94f65
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2024-06-13 21:20:48 +0900

    Time: 0 ms (100.00%), Space: 66.4 MB (95.56%) - LeetHub

---
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public int findCenter(int[][] edges) {
+        Set<Integer> set = new HashSet<>();
+        set.add(edges[0][0]);
+        set.add(edges[0][1]);
+        if (set.contains(edges[1][0])) {
+            return edges[1][0];
+        } else {
+            return edges[1][1];
+        }
+    }
+}
