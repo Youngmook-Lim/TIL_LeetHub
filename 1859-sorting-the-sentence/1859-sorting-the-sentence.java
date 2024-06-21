fd3f6d1ca22c6f203865af4abd9285e53b5cd0a6
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2024-06-21 21:29:23 +0900

    Time: 1 ms (55.45%), Space: 41.6 MB (15.77%) - LeetHub

--- 1859-sorting-the-sentence/1859-sorting-the-sentence.java
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.java
@@ -2,16 +2,17 @@ import java.util.*;
 
 class Solution {
     public String sortSentence(String s) {
-        TreeMap<Integer, String> map = new TreeMap<>();
         String[] words = s.split(" ");
+        String[] arr = new String[words.length];
+        
         for (String x : words) {
             String word = x.substring(0, x.length() - 1);
             int n = Integer.parseInt(x.substring(x.length() - 1));
-            map.put(n, word);
+            arr[n - 1] = word;
         }
         StringBuilder sb = new StringBuilder();
-        for (Integer i : map.keySet()) {
-            sb.append(map.get(i)).append(' ');
+        for (String x : arr) {
+            sb.append(x).append(' ');
         }
         sb.deleteCharAt(sb.length() - 1);
         return sb.toString();
