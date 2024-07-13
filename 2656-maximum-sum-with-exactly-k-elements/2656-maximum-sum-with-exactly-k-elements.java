10d9a2333fda0f3b6b08ece76620ade1df64f29c
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2024-07-13 19:34:38 +0900

    Time: 1 ms (100.00%), Space: 45.2 MB (8.85%) - LeetHub

--- 2656-maximum-sum-with-exactly-k-elements/2656-maximum-sum-with-exactly-k-elements.java
+++ 2656-maximum-sum-with-exactly-k-elements/2656-maximum-sum-with-exactly-k-elements.java
@@ -1,6 +1,9 @@
 class Solution {
     public int maximizeSum(int[] nums, int k) {
-        int max = Arrays.stream(nums).max().getAsInt();
+        int max = 0;
+        for (int x : nums) {
+            max = Math.max(max, x);
+        }
         return k * (2 * max + k - 1) / 2;
     }
 }
