ee2722d0cf7584c9a96fcef5d7c0f065bd6ba34a
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2024-07-18 21:06:45 +0900

    Time: 1 ms (68.45%), Space: 42.7 MB (76.97%) - LeetHub

--- 3131-find-the-integer-added-to-array-i/3131-find-the-integer-added-to-array-i.java
+++ 3131-find-the-integer-added-to-array-i/3131-find-the-integer-added-to-array-i.java
@@ -1,7 +1,12 @@
 class Solution {
     public int addedInteger(int[] nums1, int[] nums2) {
-        int a1 = Arrays.stream(nums1).min().getAsInt();
-        int a2 = Arrays.stream(nums2).min().getAsInt();
+        int a1 = Integer.MAX_VALUE;
+        int a2 = Integer.MAX_VALUE;
+        
+        for (int i = 0; i < nums1.length; i++) {
+            a1 = Math.min(a1, nums1[i]);
+            a2 = Math.min(a2, nums2[i]);
+        }
         
         return a2 - a1;
     }
