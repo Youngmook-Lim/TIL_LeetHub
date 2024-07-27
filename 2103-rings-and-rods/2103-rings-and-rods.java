b331f3061fa6130ff0adbeb01bb0da0892115bc2
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2024-07-27 18:53:46 +0900

    Time: 0 ms (100.00%), Space: 41.3 MB (65.24%) - LeetHub

--- 2103-rings-and-rods/2103-rings-and-rods.java
+++ 2103-rings-and-rods/2103-rings-and-rods.java
@@ -4,21 +4,27 @@ class Solution {
     public int countPoints(String rings) {
         int n = 10;
         int cnt = 0;
-        Set<Character>[] rods = new Set[n];
-        for (int i = 0; i < rods.length; i++) {
-            rods[i] = new HashSet<>();
-        }
+        int[] rods = new int[n];
+        
         for (int i = 0; i < rings.length(); i += 2) {
             char ring = rings.charAt(i);
             int num = rings.charAt(i + 1) - '0';
-            Set<Character> set = rods[num];
-            if (set == null) continue;
-            set.add(ring);
-            if (set.size() == 3) {
-                rods[num] = null;
-                cnt++;
+            int rgb = 0;
+            if (ring == 'R') {
+                rgb = 1;
+            } else if (ring == 'G') {
+                rgb = 2;
+            } else {
+                rgb = 4;
             }
+            
+            rods[num] = rods[num] | rgb;
         }
+        
+        for (int x : rods) {
+            if (x == 7) cnt++;
+        }
+        
         return cnt;
     }
 }
