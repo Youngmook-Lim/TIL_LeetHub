0eb085e79836f3072852d0df45e8c41e5a6eac11
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2024-08-02 21:06:58 +0900

    Time: 1 ms (100.00%), Space: 41.9 MB (79.51%) - LeetHub

---
+++ 3168-minimum-number-of-chairs-in-a-waiting-room/3168-minimum-number-of-chairs-in-a-waiting-room.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int minimumChairs(String s) {
+        int cnt = 0;
+        int max = 0;
+        for (char c : s.toCharArray()) {
+            if (c == 'E') {
+                cnt++;
+                max = Math.max(max, cnt);
+            } else {
+                cnt--;
+            }
+        }
+        return max;
+    }
+}
