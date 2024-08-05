5ea6db36e352e338cc1f9fe7a8331673139b0957
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2024-08-05 20:06:45 +0900

    Time: 0 ms (100.00%), Space: 40.3 MB (56.04%) - LeetHub

--- 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.java
+++ 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.java
@@ -2,12 +2,6 @@ import java.util.*;
 
 class Solution {
     public boolean isSameAfterReversals(int num) {
-        StringBuilder sb = new StringBuilder(Integer.toString(num));
-        String orgStr = sb.toString();
-        sb.reverse();
-        String newStr = sb.toString();
-        String tmp = Integer.toString(Integer.parseInt(sb.toString()));
-        String newOrgStr = new StringBuilder(tmp).reverse().toString();
-        return orgStr.equals(newOrgStr);
+        return num == 0 ? true : num % 10 != 0;
     }
 }
