c1dc6f45849fe35f3e46c2d740feb68eb44fcbf0
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2024-08-12 22:00:13 +0900

    Time: 13 ms (6.50%), Space: 44.7 MB (12.51%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.java
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.java
@@ -1,39 +1,13 @@
 class Solution {
     
-    class P implements Comparable<P> {
-        int n, cnt;
-        
-        P(int n, int cnt) {
-            this.n = n;
-            this.cnt = cnt;
-        }
-        
-        @Override
-        public int compareTo(P p) {
-            if (this.cnt != p.cnt) {
-                return this.cnt - p.cnt;            
-            }
-            return p.n - this.n;
-        }
-    }
-    
     public int[] frequencySort(int[] nums) {
         Map<Integer, Integer> map = new HashMap<>();
-        for (int x : nums) {
-            map.put(x, map.getOrDefault(x, 0) + 1);
-        }
-        List<P> list = new ArrayList<>();
-        for (Integer x : map.keySet()) {
-            int cnt = map.get(x);
-            for (int i = 0; i < cnt; i++) {
-                list.add(new P(x, cnt));                
-            }
-        }
-        Collections.sort(list);
-        int[] ans = new int[nums.length];
-        for (int i = 0; i < list.size(); i++) {
-            ans[i] = list.get(i).n;
-        }
-        return ans;
+        Arrays.stream(nums).forEach(x -> map.put(x, map.getOrDefault(x, 0) + 1));
+
+        return Arrays.stream(nums)
+                     .boxed()
+                     .sorted((a, b) -> map.get(a) != map.get(b) ? map.get(a) - map.get(b) : b - a)
+                     .mapToInt(n -> n)
+                     .toArray();
     }
 }
