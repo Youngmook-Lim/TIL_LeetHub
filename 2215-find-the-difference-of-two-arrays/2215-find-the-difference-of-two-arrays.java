ae7271ff96b056b6a864a4788109416da25b94cd
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2024-08-16 14:28:06 +0900

    Time: 2 ms (99.85%), Space: 45.4 MB (57.07%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.java
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.java
@@ -2,41 +2,33 @@ import java.util.*;
 
 class Solution {
     public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
-        Set<Integer> set1 = new HashSet<>();
-        Set<Integer> set2 = new HashSet<>();
-        for (int x : nums1) {
-            set1.add(x);
-        } 
-        for (int x : nums2) {
-            set2.add(x);
-        }
-        
-        List<Integer> res1 = Arrays.stream(nums1)
-            .filter(v -> {
-                if (set2.contains(v)) {
-                    return false;
-                } else {
-                    set2.add(v);
-                    return true;                    
-                }
-            })
-            .boxed()
-            .collect(Collectors.toList());
-        List<Integer> res2 = Arrays.stream(nums2)
-            .filter(v -> {
-                if (set1.contains(v)) {
-                    return false;
-                } else {
-                    set1.add(v);
-                    return true;                    
-                }
-            })
-            .boxed()
-            .collect(Collectors.toList());
+        boolean[] arr1 = new boolean[2001];
+        boolean[] arr2 = new boolean[2001];
         
         List<List<Integer>> list = new ArrayList<>();
-        list.add(res1);
-        list.add(res2);
+        list.add(new ArrayList<>());
+        list.add(new ArrayList<>());
+        
+        for (int x : nums1) {
+            arr1[x + 1000] = true;
+        }
+        for (int x : nums2) {
+            arr2[x + 1000] = true;
+        }
+        
+        for (int x : nums1) {
+            if (arr1[x + 1000] && !arr2[x + 1000]) {
+                arr1[x + 1000] = false;
+                list.get(0).add(x);
+            }
+        }
+        
+        for (int x : nums2) {
+            if (arr2[x + 1000] && !arr1[x + 1000]) {
+                arr2[x + 1000] = false;
+                list.get(1).add(x);
+            }
+        }
         
         return list;
     }
