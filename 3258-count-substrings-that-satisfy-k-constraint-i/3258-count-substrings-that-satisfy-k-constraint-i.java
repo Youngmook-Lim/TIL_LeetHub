6f5846c251774696bb20cde7ad8c2b2016695e1c
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2024-08-21 22:53:22 +0900

    Time: 1 ms (100.00%), Space: 41.4 MB (99.95%) - LeetHub

--- 3258-count-substrings-that-satisfy-k-constraint-i/3258-count-substrings-that-satisfy-k-constraint-i.java
+++ 3258-count-substrings-that-satisfy-k-constraint-i/3258-count-substrings-that-satisfy-k-constraint-i.java
@@ -1,57 +1,34 @@
 class Solution {
     public int countKConstraintSubstrings(String s, int k) {
         int left = 0;
-        int right = 1;
+        int right = 0;
         int one = 0;
         int zero = 0;
         int n = s.length();
         int cnt = 0;
-        if (s.charAt(left) == '1') {
-            one++;
-        } else {
-            zero++;
-        }
-        while (left < n) {
-            
-            if (one <= k || zero <= k) {
-                right++;
-                if (right == n + 1) {
-                    left++;
-                    right = left + 1;
-                    one = 0;
-                    zero = 0;
-                    if (left < n) {
-                        if (s.charAt(left) == '1') {
-                            one++;
-                        } else {
-                            zero++;
-                        }
-                    }
-                } else {
-                    if (s.charAt(right - 1) == '1') {
-                        one++;
-                    } else {
-                        zero++;
-                    }
-                }
-                cnt++;
+        
+        while (right < n) {
+            if (s.charAt(right) == '0') {
+                zero++;
             } else {
-                left++;
-                right = left + 1;
-                one = 0;
-                zero = 0;
-                if (left < n) {
-                    if (s.charAt(left) == '1') {
-                        one++;
-                    } else {
-                        zero++;
-                    }
-                }
+                one++;
             }
             
-
+            while (one > k && zero > k) {
+                if (s.charAt(left) == '0') {
+                    zero--;
+                } else {
+                    one--;
+                }
+                left++;
+            }
             
+            cnt += right - left + 1;
+            
+            right++;
         }
+        
+        
         return cnt;
     }
 }
