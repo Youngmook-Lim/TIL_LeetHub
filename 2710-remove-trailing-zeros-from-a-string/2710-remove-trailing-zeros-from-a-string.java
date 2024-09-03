7781aa30daf37542b04093389497d4d23d9fb4fa
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2024-09-03 09:24:07 +0900

    Time: 1 ms (100.00%), Space: 44.6 MB (75.62%) - LeetHub

--- 2710-remove-trailing-zeros-from-a-string/2710-remove-trailing-zeros-from-a-string.java
+++ 2710-remove-trailing-zeros-from-a-string/2710-remove-trailing-zeros-from-a-string.java
@@ -1,16 +1,14 @@
 class Solution {
     public String removeTrailingZeros(String num) {
-        StringBuilder sb = new StringBuilder();
-        boolean flag = false;
+        int idx = num.length() - 1;
         
         for (int i = num.length() - 1; i >= 0; i--) {
             if (num.charAt(i) != '0') {
-                flag = true;
-            }
-            if (flag) {
-                sb.append(num.charAt(i));
+                idx = i;
+                break;
             }
         }
-        return sb.reverse().toString();
+        
+        return num.substring(0, idx + 1);
     }
 }
