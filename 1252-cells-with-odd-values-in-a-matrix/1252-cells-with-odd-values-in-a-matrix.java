e185d14866a3dae0cd24f9eca58902bac1c11614
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2024-09-05 21:35:38 +0900

    Time: 4 ms (12.02%), Space: 42 MB (14.13%) - LeetHub

--- 1252-cells-with-odd-values-in-a-matrix/1252-cells-with-odd-values-in-a-matrix.java
+++ 1252-cells-with-odd-values-in-a-matrix/1252-cells-with-odd-values-in-a-matrix.java
@@ -1,24 +1,27 @@
 class Solution {
     public int oddCells(int m, int n, int[][] indices) {
         int[][] graph = new int[m][n];
+        int cnt = 0;
         
         for (int[] arr : indices) {
             for (int i = 0; i < m; i++) {
                 graph[i][arr[1]]++;
+                if (graph[i][arr[1]] % 2 == 0) {
+                    cnt--;
+                } else {
+                    cnt++;
+                }
             }
             for (int i = 0; i < n; i++) {
                 graph[arr[0]][i]++;
+                if (graph[arr[0]][i] % 2 == 0) {
+                    cnt--;
+                } else {
+                    cnt++;
+                }
             }
         }
-        
-        int cnt = 0;
-        
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (graph[i][j] % 2 != 0) cnt++;
-            }
-        }
-        
+    
         return cnt;
     }
 }
