dfd9ea0295095b36b2a0ee97c3020b4756cab88b
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2024-09-06 21:47:09 +0900

    Time: 2 ms (94.74%), Space: 46.4 MB (71.86%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -2,19 +2,12 @@ class Solution {
     public int[] countBits(int n) {
         int[] arr = new int[n + 1];
         
-        for (int i = 0; i <= n; i++) {
-            arr[i] = getOnes(i);
+        for (int i = 1; i <= n; i++) {
+            arr[i] = arr[i >> 1] + (i & 1);
         }
         
         return arr;
     }
     
-    public int getOnes(int n) {
-        int len = Integer.toBinaryString(n).length();
-        int cnt = 0;
-        for (int i = 0; i < len; i++) {
-            if ((n & (1 << i)) > 0) cnt++;
-        }
-        return cnt;
-    }
+
 }
