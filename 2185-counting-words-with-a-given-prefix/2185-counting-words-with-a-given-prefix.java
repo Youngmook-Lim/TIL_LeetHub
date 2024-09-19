3e834abce8ab44aced9ee44c9fdd6a59bbc5f7c5
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2024-09-20 01:03:46 +0900

    Time: 1 ms (38.24%), Space: 43 MB (35.02%) - LeetHub

--- 2185-counting-words-with-a-given-prefix/2185-counting-words-with-a-given-prefix.java
+++ 2185-counting-words-with-a-given-prefix/2185-counting-words-with-a-given-prefix.java
@@ -3,11 +3,7 @@ class Solution {
         int cnt = 0;
         loop:
         for (String x : words) {
-            if (x.length() < pref.length()) continue;
-            for (int i = 0; i < pref.length(); i++) {
-                if (pref.charAt(i) != x.charAt(i)) continue loop;
-            }
-            cnt++;
+            if (x.length() >= pref.length() && pref.equals(x.substring(0, pref.length()))) cnt++;
         }
         return cnt;
     }
