75aef88c6ca12a6f51e174c8bc9eafdb090f099b
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2024-10-13 14:33:08 +0900

    Time: 7 ms (33.45%), Space: 43.4 MB (62.97%) - LeetHub

--- 1370-increasing-decreasing-string/1370-increasing-decreasing-string.java
+++ 1370-increasing-decreasing-string/1370-increasing-decreasing-string.java
@@ -1,18 +1,27 @@
 class Solution {
     public String sortString(String s) {
         int[] arr = new int[26];
+        List<Integer> list = new ArrayList<>();
+        
         for (char c : s.toCharArray()) {
             arr[c - 'a']++;
+            if (arr[c - 'a'] == 1) {
+                list.add(c - 'a');
+            }
         }
+        
+        Collections.sort(list);
         
         boolean flag = true;
         boolean toggle = false;
         StringBuilder sb = new StringBuilder();
         
         while (flag) {
+            
             flag = false;
-            for (int i = 0; i < 26; i++) {
-                int idx = toggle ? 25 - i : i;
+            for (int i = 0; i < list.size(); i++) {
+                int idx = toggle ? list.get(list.size() - 1 - i) : list.get(i);
+                if (idx == -1) continue;
                 if (arr[idx] == 0) continue;
                 sb.append((char) ('a' + idx));
                 arr[idx]--;
