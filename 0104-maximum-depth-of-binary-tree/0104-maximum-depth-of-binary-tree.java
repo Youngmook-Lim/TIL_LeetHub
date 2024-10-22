ca6a697ed854041af6a3db8dcbddf09743118145
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2024-10-22 22:30:35 +0900

    Time: 0 ms (100.00%), Space: 42.3 MB (87.57%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -19,10 +19,6 @@ class Solution {
             return 0;
         }
         
-        if (root.left == null && root.right == null) {
-            return 1;
-        }
-        
         int left = maxDepth(root.left);
         int right = maxDepth(root.right);
         
