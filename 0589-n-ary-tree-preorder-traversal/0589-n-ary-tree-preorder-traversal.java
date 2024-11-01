d2d43f97f9207e6933a28bf7932de6df3adb406e
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2024-11-01 22:54:01 +0900

    Time: 2 ms (21.67%), Space: 45.8 MB (5.07%) - LeetHub

--- 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.java
+++ 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.java
@@ -24,6 +24,7 @@ class Solution {
         
         list.add(root.val);
         for (Node n : root.children) {
+            if (n == null) continue;
             list.addAll(preorder(n));
         }
         return list;
