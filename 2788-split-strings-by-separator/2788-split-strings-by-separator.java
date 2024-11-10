aa39fb63431c4e2db24268b47ff1ab4f18c58bb5
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2024-11-10 13:18:06 +0900

    Time: 23 ms (31.96%), Space: 45.8 MB (37.43%) - LeetHub

--- 2788-split-strings-by-separator/2788-split-strings-by-separator.java
+++ 2788-split-strings-by-separator/2788-split-strings-by-separator.java
@@ -2,7 +2,7 @@ class Solution {
     public List<String> splitWordsBySeparator(List<String> words, char separator) {
         List<String> list = new ArrayList<>();
         for (String s : words) {         
-            String[] tmp = s.split("[" + Character.toString(separator) + "]");
+            String[] tmp = s.split("[" + separator + "]");
             for (String str : tmp) {
                 if (!str.equals("")) {
                     list.add(str);
