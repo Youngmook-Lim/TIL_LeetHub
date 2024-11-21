245c8f9ca3922b43feab1fa23ec908d3349bac87
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2024-11-21 23:50:58 +0900

    Time: 12 ms (21.45%), Space: 44.7 MB (95.15%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int n : nums) {
+            if (set.contains(n)) {
+                set.remove(n);
+            } else {
+                set.add(n);
+            }
+        }
+        
+        for (int n : set) {
+            return n;
+        }
+        return -1;
+    }
+}
