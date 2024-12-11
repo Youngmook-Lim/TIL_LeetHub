2a370b75bff9cc56da4427b8298e11ad57d79dc2
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2024-12-11 22:52:41 +0900

    Time: 2 ms (76.92%), Space: 44.1 MB (61.01%) - LeetHub

--- 0883-projection-area-of-3d-shapes/0883-projection-area-of-3d-shapes.java
+++ 0883-projection-area-of-3d-shapes/0883-projection-area-of-3d-shapes.java
@@ -4,20 +4,15 @@ class Solution {
         int n = grid.length;
         
         for (int i = 0; i < n; i++) {
-            int max = 0;
+            int max1 = 0;
+            int max2 = 0;
             for (int j = 0; j < n; j++) {
-                max = Math.max(max, grid[i][j]);
+                max1 = Math.max(max1, grid[i][j]);
+                max2 = Math.max(max2, grid[j][i]);
                 if (grid[i][j] != 0) total++;
             }
-            total += max;
-        }
-        
-        for (int i = 0; i < n; i++) {
-            int max = 0;
-            for (int j = 0; j < n; j++) {
-                max = Math.max(max, grid[j][i]);
-            }
-            total += max;
+            total += max1;
+            total += max2;
         }
         
         return total;
