3d15bfb23590b1e1e3e4242798281e4a65bc69da
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2024-12-18 15:46:15 +0900

    Time: 0 ms (100.00%), Space: 43.5 MB (33.89%) - LeetHub

--- 2255-count-prefixes-of-a-given-string/2255-count-prefixes-of-a-given-string.java
+++ 2255-count-prefixes-of-a-given-string/2255-count-prefixes-of-a-given-string.java
@@ -1,17 +1,8 @@
 class Solution {
     public int countPrefixes(String[] words, String s) {
         int cnt = 0;
-        int len = s.length();
         for (String str : words) {
-            if (len < str.length()) continue;
-            boolean flag = false;
-            for (int i = 0; i < str.length(); i++) {
-                if (str.charAt(i) != s.charAt(i)) {
-                    flag = true;
-                    break;
-                }
-            }
-            if (!flag) cnt++;
+            if (s.startsWith(str)) cnt++;
         }
         return cnt;
     }
