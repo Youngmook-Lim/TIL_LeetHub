b2106414f1c03c7bf01675dc37e389cea065a19b
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2025-01-11 12:08:22 +0900

    Time: 0 ms (100%), Space: 41.2 MB (65.2%) - LeetHub

---
+++ 1329-minimum-cost-to-move-chips-to-the-same-position/1329-minimum-cost-to-move-chips-to-the-same-position.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int minCostToMoveChips(int[] position) {
+        int even = 0;
+        int odd = 0;
+        for (int n : position) {
+            if (n % 2 == 0) {
+                even++;
+            } else {
+                odd++;
+            }
+        }
+        return Math.min(odd, even);
+        
+    }
+}
