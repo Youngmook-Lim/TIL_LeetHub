d3fdc43513d9add8da3338ada602865ed7a93e25
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2025-01-23 00:00:33 +0900

    Time: 51 ms (10.74%), Space: 23.8 MB (20.68%) - LeetHub

--- 2132-convert-1d-array-into-2d-array/2132-convert-1d-array-into-2d-array.py
+++ 2132-convert-1d-array-into-2d-array/2132-convert-1d-array-into-2d-array.py
@@ -1,9 +1,11 @@
 class Solution:
     def construct2DArray(self, original: List[int], m: int, n: int) -> List[List[int]]:
-        graph = [[0 for _ in range(n)] for _ in range(m)]
+        
         target = n * m
         if target != len(original):
             return []
+
+        graph = [[0 for _ in range(n)] for _ in range(m)]
 
         for i in range(len(original)):
             y = i // n
