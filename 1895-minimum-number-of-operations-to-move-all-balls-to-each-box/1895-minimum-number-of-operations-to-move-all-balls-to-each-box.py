7e514d2b30aaedfac9830d5a8e68fbc9f6e0f4bb
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2025-02-21 18:44:41 +0900

    Time: 13 ms (46.44%), Space: 18 MB (76.26%) - LeetHub

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.py
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.py
@@ -1,18 +1,19 @@
 class Solution:
     def minOperations(self, boxes: str) -> List[int]:
         n = len(boxes)
-        forward = [0 for _ in range(n + 1)]
-        backward = [0 for _ in range(n + 1)]
+        forward = [0] * n
+        backward = [0] * n
         forward_cnt = 0
         backward_cnt = 0
-        res = [0 for _ in range(n)]
 
-        for i in range(1, n + 1):
-            forward[i] = forward[i - 1] + int(boxes[i - 1]) + forward_cnt
-            backward[i] = backward[i - 1] + int(boxes[n - i]) + backward_cnt
-            if (int(boxes[i - 1]) == 1):
+        res = [0] * n
+
+        for i in range(1, n):
+            forward[i] = forward[i - 1] + (1 if boxes[i - 1] == "1" else 0) + forward_cnt
+            backward[i] = backward[i - 1] + (1 if boxes[n - i] == "1" else 0) + backward_cnt
+            if (boxes[i - 1] == "1"):
                 forward_cnt += 1
-            if (int(boxes[n - i]) == 1):
+            if (boxes[n - i] == "1"):
                 backward_cnt += 1
 
         for i in range(n):
