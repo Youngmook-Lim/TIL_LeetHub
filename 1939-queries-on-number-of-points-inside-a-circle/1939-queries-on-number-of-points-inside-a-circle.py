b9d1161ca8df1d290fc74ef6e9b6fb9c3b659269
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2025-03-12 21:49:10 +0900

    Time: 511 ms (47.51%), Space: 18.1 MB (42.26%) - LeetHub

--- 1939-queries-on-number-of-points-inside-a-circle/1939-queries-on-number-of-points-inside-a-circle.py
+++ 1939-queries-on-number-of-points-inside-a-circle/1939-queries-on-number-of-points-inside-a-circle.py
@@ -1,6 +1,7 @@
 class Solution:
     def countPoints(self, points: List[List[int]], queries: List[List[int]]) -> List[int]:
         ret: List[int] = []
+        points.sort(key=lambda x: x[0])
 
         def get_distance(x1, y1, x0, y0):
             return math.sqrt((y1 - y0) ** 2 + (x1 - x0) ** 2)
@@ -11,7 +12,13 @@ class Solution:
             r = q[2]
             cnt = 0
             for p in points:
-                dist = get_distance(x, y, p[0], p[1])
+                px = p[0]
+                py = p[1]
+                if px + r < x:
+                    continue
+                if px - r > x:
+                    break
+                dist = get_distance(x, y, px, py)
                 if (dist <= r):
                     cnt += 1
             ret.append(cnt)
