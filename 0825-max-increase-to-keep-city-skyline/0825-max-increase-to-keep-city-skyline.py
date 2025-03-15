4fe86d44def23f3786fd54912ba9960ba09471bc
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2025-03-15 12:14:16 +0900

    Time: 3 ms (95.81%), Space: 17.7 MB (91.62%) - LeetHub

--- 0825-max-increase-to-keep-city-skyline/0825-max-increase-to-keep-city-skyline.py
+++ 0825-max-increase-to-keep-city-skyline/0825-max-increase-to-keep-city-skyline.py
@@ -1,27 +1,7 @@
 class Solution:
     def maxIncreaseKeepingSkyline(self, grid: List[List[int]]) -> int:
-        max_x = []
-        max_y = []
-        n = len(grid)
-        total = 0
+        max_x, max_y = list(map(max, grid)), list(map(max, zip(*grid)))
 
-        def get_max(grid, n, k, type):
-            maxi = 0
-            for i in range(n):
-                if type == "x":
-                    maxi = max(maxi, grid[k][i])
-                else:
-                    maxi = max(maxi, grid[i][k])
-            return maxi
-
-        for i in range(n):
-            max_x.append(get_max(grid, n, i, "x"))
-            max_y.append(get_max(grid, n, i, "y"))
+        return sum(min(x, y) for x in max_x for y in max_y) - sum(map(sum, grid))
         
-        for i in range(n):
-            for j in range(n):
-                mini = min(max_x[j], max_y[i])
-                total += mini - grid[i][j]
-        
-        return total
 
