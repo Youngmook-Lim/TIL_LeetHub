b3732f9083e63f378e582aee3e5df457d72a4ca3
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2025-03-29 21:33:27 +0900

    Time: 7 ms (73.24%), Space: 17.7 MB (90.04%) - LeetHub

--- 1553-count-triplets-that-can-form-two-arrays-of-equal-xor/1553-count-triplets-that-can-form-two-arrays-of-equal-xor.py
+++ 1553-count-triplets-that-can-form-two-arrays-of-equal-xor/1553-count-triplets-that-can-form-two-arrays-of-equal-xor.py
@@ -7,11 +7,10 @@ class Solution:
 
         for i in range(2, n + 1):
             cum[i] = cum[i - 1] ^ arr[i - 1]
-            
+
         for i in range(n + 1):
             for k in range(i + 1, n + 1):
                 if cum[i] == cum[k]:
-                    print(i, k)
                     cnt += (k - i - 1)
         
         return cnt
