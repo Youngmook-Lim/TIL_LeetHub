c5c9367ea912a74bdbf02e6e6b7f3673e938b2be
Youngmook Lim <94703258+Youngmook-Lim@users.noreply.github.com>
2025-04-01 19:48:06 +0900

    Time: 9 ms (88.03%), Space: 18.1 MB (40.17%) - LeetHub

--- 1525-queries-on-a-permutation-with-key/1525-queries-on-a-permutation-with-key.py
+++ 1525-queries-on-a-permutation-with-key/1525-queries-on-a-permutation-with-key.py
@@ -1,15 +1,48 @@
+from typing import List
+
+class BIT:
+    def __init__(self, n: int):
+        self.n = n
+        self.tree = [0] * (n + 1)
+    
+    def update(self, i: int, delta: int) -> None:
+        while i <= self.n:
+            self.tree[i] += delta
+            i += i & -i  # Move to next index
+    
+    def query(self, i: int) -> int:
+        s = 0
+        while i:
+            s += self.tree[i]
+            i -= i & -i  # Move to parent index
+        return s
+
 class Solution:
     def processQueries(self, queries: List[int], m: int) -> List[int]:
-        dq = deque()
-        res = []
-
+        n = len(queries)
+        size = m + n  # Extra space to allow moving elements to the front
+        bit = BIT(size)
+        
+        # pos[i] will store the current index of element i
+        pos = [0] * (m + 1)
         for i in range(1, m + 1):
-            dq.append(i)
+            pos[i] = n + i  # Place each element starting from index n+1
+            bit.update(n + i, 1)
+        
+        result = []
+        current_top = n  # Next available position for moving an element to the front
         
         for q in queries:
-            idx = dq.index(q)
-            res.append(idx)
-            dq.remove(q)
-            dq.appendleft(q)
-        
-        return res
+            original_pos = pos[q]
+            # Get the number of elements before the current element q
+            rank = bit.query(original_pos) - 1
+            result.append(rank)
+            
+            # Remove element q from its current position
+            bit.update(original_pos, -1)
+            # Move q to the front (using current_top index)
+            pos[q] = current_top
+            bit.update(current_top, 1)
+            current_top -= 1
+            
+        return result
